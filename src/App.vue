2cf36c047e53b6b443faea9e44d15569f01f3623
jeongsoo <rpsha33@naver.com>
2020-05-28 11:41:19 +0900

    tdd todo list

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,16 @@
       <li
           v-for="(item, index) in todoList"
           :key="index">
+          <button class="increse" @click="addQuantity(index)">+</button>
+          {{item.quantity}}
+          <button class="decrese" @click="removeQuantity(index)">-</button>
           {{item.text}}
+          <button class="removeBtn" @click="removeItem(index)">X</button>
       </li>
     </ul>
+    <div class="totalCount">
+      {{totalCount}}
+    </div>
   </div>
 </template>
 
@@ -21,20 +28,33 @@ export default {
   data() {
     return {
       todoList: [
-        { text: 'good' },
+        { text: 'good', quantity: 3 },
+        { text: 'nice', quantity: 5 },
+        { text: 'great', quantity: 1 },
       ],
       txt: '',
+      countTotal: 0,
     };
   },
   methods: {
     addItem() {
-      if (!this.txt) {
-        return;
-      }
-      this.todoList.push(this.txt);
+      if (!this.txt) return;
+      this.todoList.push({ text: this.txt });
     },
-    removeItem() {
-      this.todoList.push(this.txt);
+    removeItem(index) {
+      this.todoList.splice(index, 1);
+    },
+    addQuantity(index) {
+      this.todoList[index].quantity += 1;
+    },
+    removeQuantity(index) {
+      this.todoList[index].quantity -= 1;
+    },
+  },
+  computed: {
+    totalCount() {
+      const total = this.todoList.reduce((acc, val) => acc + val.quantity, 0);
+      return total;
     },
   },
 };
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,23 +3,43 @@ import App from '@/App.vue';
 
 let app;
 let data;
-describe('vue Test', () => {
+let totalCount;
+const mockcallback = jest.fn((f) => f + 2);
+describe('Test', () => {
   beforeEach(() => { // it 테스트 전에 정의할 값들을 만들때 좋다. DRY로 중복되는 코드를 한번에 쓰자.
     app = shallowMount(App);
     data = app.vm.$data;
+    totalCount = app.find('.totalCount');
   });
 
-  it('addBtn Click todoList add Test', () => {
-    const input = app.find('.inputTxt');
-    const addbtn = app.find('.addBtn');
-    input.setValue('gg');
-    addbtn.trigger('click');
-    expect(data.todoList).toEqual(['gg']);
-  });
+  // it('addBtn Click todoList item add Test', () => {
+  //   const input = app.find('.inputTxt');
+  //   const addbtn = app.find('.addBtn');
+  //   input.setValue('gg');
+  //   addbtn.trigger('click');
+  //   expect(data.todoList).toEqual([{ text: 'good' }, { text: 'gg' }]);
+  // });
 
-  it('removeBtn Click todoList remove Test', () => {
-    const btn = app.find('.removeBtn');
+  // it('removeBtn Click todoList item remove Test', () => {
+  //   const btn = app.find('.removeBtn');
+  //   btn.trigger('click');
+  //   expect(data.todoList).toEqual([]);
+  // });
+
+  it('target item Quantity increse', () => {
+    const btn = app.find('.increse');
     btn.trigger('click');
-    expect(data.todoList).toEqual([]);
+    expect(data.todoList[0].quantity).toBe(4);
+  });
+
+  it('target item Quantity decrese', () => {
+    const btn = app.find('.decrese');
+    btn.trigger('click');
+    expect(data.todoList[0].quantity).toBe(2);
+  });
+
+  it('mockTest', () => {
+    [1].forEach(mockcallback);
+    expect(mockcallback.mock.results[0].value).toBe(3);
   });
 });
