749c5612575e724f18f0cb44aa981f2fe34e1d6e
jeongsoo <rpsha33@naver.com>
2020-07-15 10:59:52 +0900

    퍼블

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,17 +1,48 @@
 <template>
   <header class="header">
-    <h1>COLOR PICKER</h1>
+    <h1 class="logo_title">
+      <a href="/">로고</a>
+    </h1>
+    <h2 class="title">
+      {{title}}
+    </h2>
+    <nav class="nav_hader">
+      <button class="" @click="showMenu">햄버거</button>
+      <ul class="list_menu">
+        <li class="">Color Picker</li>
+      </ul>
+    </nav>
   </header>
 </template>
 
+<script>
+export default {
+  name: 'header',
+  data() {
+    return {
+      title: 'Color Picker',
+    };
+  },
+  methods: {
+    showMenu() {
+      console.log(123);
+    },
+  },
+  computed: {},
+};
+</script>
+
 <style>
 .header {
-  background-color: darkgray;
-  width: 100%;
+  display: flex;
+  padding: 0 30px;
+  border-bottom: solid 1px #d1d2d6;
+  justify-content: space-between;
   line-height: 50px;
-  text-align: center;
 }
-.header div {
-  text-align: center;
+
+.header .nav_hader .list_menu{
+  display: none;
 }
+
 </style>
