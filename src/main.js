977890de857318be13616cc25177bde3b3c8d194
jeongsso <rpsha33@naver.com>
2020-07-17 22:58:10 +0900

    router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
@@ -6,3 +7,4 @@ Vue.config.productionTip = false;
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
+Vue.use(VueRouter);
